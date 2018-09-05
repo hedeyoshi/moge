af4ce5ffa01a3eed554f8dd8a3b9b2a2e52e0346
hedeyoshi <h158cm@gmail.com>
2018-09-05 14:01:32 +0900

    #2 Footer追加～読み込ませるまで

--- src/views/Moge.vue
+++ src/views/Moge.vue
@@ -3,6 +3,7 @@
     <Header logo="MogeMoge"/>
     <h1>This is an Moge page</h1>
     <HelloWorld msg="もげもげもげもげ"/>
+    <Footer logo="moge footer" />
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
 import HelloWorld from '@/components/HelloWorld.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'moge',
   components: {
     Header,
-    HelloWorld
+    HelloWorld,
+    Footer
   }
 }
 </script>
